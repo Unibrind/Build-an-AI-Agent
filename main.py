18f49b2dd503c719e0e62f45f59c7096d4d16ed2
unibrind <mebarekouldmammar95@gmail.com>
2026-02-11 10:34:32 +0100

    1-feat: add CLI argument parsing and token usage tracking

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
@@ -11,13 +12,25 @@ if api_key is None:
 client = genai.Client(api_key=api_key)
 
 def main():
-    print("Hello from Unibrind-AI-Agent!")
+    # Now we can access `args.user_prompt`
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt", metavar="user_prompt, ex: uv run main.py 'user_prompt'")
+    args = parser.parse_args()
+    # Now we can access `args.user_prompt`
+    
+    
     # gemini-2.5-flash
     generate_content = client.models.generate_content(
-        model='gemini-2.5-flash',
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    )
-    print(generate_content.text)
+        model = 'gemini-2.5-flash',
+        contents = args.user_prompt)
+    
+    if generate_content.usage_metadata is None:
+        raise RuntimeError("Failed to retrieve token usage metadata from Gemini response.")
+    else:
+        print("\nHello from Unibrind-AI-Agent!\n")
+        print("Prompt tokens: ", generate_content.usage_metadata.prompt_token_count)
+        print("Response tokens: ", generate_content.usage_metadata.candidates_token_count)
+        print("\n", generate_content.text)
 
 
 if __name__ == "__main__":
